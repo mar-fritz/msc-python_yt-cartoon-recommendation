4a7a7fd4473646c4463bef93f3dc2647ac3a92c0
mar-fritz <mjavery787@gmail.com>
2021-01-21 14:11:13 +0200

    Add Indicators analysis function

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import mysql.connector
 import db_operations
 import yt_requests
+import indicators
 import time
 
 db_username = "root"
 db_pass = "x"
 db_name = "test"
+db_host = "127.0.0.1"
+db_port = "3306"
 
 
 def get_statistics(df_in, api_key, time_interval=3600, iterations=48):
@@ -15,7 +18,7 @@ def get_statistics(df_in, api_key, time_interval=3600, iterations=48):
         statistics_df = yt_requests.get_views_likes_dislikes(api_key=api_key, df_in1=df_in)
         try:
             # Connect to mysql server
-            cnx = db_operations.db_connect(db_username, db_pass, "127.0.0.1", "3306", db_name)
+            cnx = db_operations.db_connect(db_username, db_pass, db_host, db_port, db_name)
         except mysql.connector.Error as err:
             print(err)
             print("Connection to db failed")
@@ -38,7 +41,7 @@ def db_initialization(api_key, file_path):
     video_dataframe = yt_requests.get_videos(api_key, file_path)
     try:
         # Connect to mysql server
-        cnx = db_operations.db_connect(db_username, db_pass, "127.0.0.1", "3306")
+        cnx = db_operations.db_connect(db_username, db_pass, db_host, db_port)
         # Create db and tables
         db_operations.db_setup(cnx, db_name, db_operations.tables())
     except mysql.connector.Error as err:
@@ -59,5 +62,37 @@ def db_initialization(api_key, file_path):
         print("Get statistics COMPLETE")
 
 
+def indicators_analysis():
+    # Connect to mysql server
+    cnx = db_operations.db_connect(db_username, db_pass, db_host, "3306", db_name)
+    # query data from statistics table
+    latest_stats = db_operations.fetch_data(cnx, 'statistics', date_time='latest')
+    earliest_stats = db_operations.fetch_data(cnx, 'statistics', date_time='earliest')
+    # calculate indicators
+    indicators_df = indicators.calc(earliest_stats, latest_stats)
+    # TODO: save indicators data to db
+    # # save as csv to analyze later
+    # indicators_df.to_csv("indicators.csv")
+
+    # Plot views, likes and dislikes of top 3 and bottom 3 videos according to ci indicator
+    # get ids of videos with the top 3 and bottom 3 ci values
+    ci = indicators_df['ci'].sort_values()
+    top3, bottom3 = ci.iloc[:3].index.tolist(), ci.iloc[-3:].index.tolist()
+    for video_id in top3 + bottom3:
+        video_stats = db_operations.fetch_data(cnx, 'statistics', v_id=video_id)
+        indicators.plot_views_likes_dislikes(video_stats)
+
+    # Calculate and plot correlation matrix of indicators and video duration
+    video_durations = db_operations.fetch_data(cnx, 'videos').drop(columns=['title', 'url', 'captions'])
+    indicators.correlation(indicators_df, video_durations)
+
+    # close the connection
+    cnx.close()
+
+
 if __name__ == '__main__':
-    print("test stuff here")
+    # print("test stuff here")
+    # STEP 1
+    db_initialization('your-api-key-here', "your/path/here")
+    # STEP 3
+    indicators_analysis()
